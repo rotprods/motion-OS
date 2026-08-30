8ba5425fe27278cbd08e57296c759812d55f25de
Roberto Ortega <261643698+rotprods@users.noreply.github.com>
2026-08-30 14:00:19 +0200

    feat(remotion): register S04 golden-scene composition

--- runtime/remotion/src/Root.tsx
+++ runtime/remotion/src/Root.tsx
@@ -2,16 +2,32 @@ import React from 'react';
 import {Composition} from 'remotion';
 import spec from './runtimeSpec.json';
 import {MotionOSRuntime} from './MotionOSRuntime';
+import {
+  S04Cientificamente,
+  S04_DEFAULT_PROPS,
+  S04_SPEC,
+} from './golden_s04';
 
 export const RemotionRoot: React.FC = () => {
   return (
-    <Composition
-      id="MotionOSRuntime"
-      component={MotionOSRuntime}
-      durationInFrames={spec.project.duration_frames}
-      fps={spec.project.fps}
-      width={spec.project.width}
-      height={spec.project.height}
-    />
+    <>
+      <Composition
+        id="MotionOSRuntime"
+        component={MotionOSRuntime}
+        durationInFrames={spec.project.duration_frames}
+        fps={spec.project.fps}
+        width={spec.project.width}
+        height={spec.project.height}
+      />
+      <Composition
+        id="GoldenS04Cientificamente"
+        component={S04Cientificamente}
+        durationInFrames={S04_SPEC.local.frameCount}
+        fps={S04_SPEC.source.fps}
+        width={S04_SPEC.source.width}
+        height={S04_SPEC.source.height}
+        defaultProps={S04_DEFAULT_PROPS}
+      />
+    </>
   );
 };
