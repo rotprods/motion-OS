5f162872a45c40dc2e65f02e58186c10356dbf7c
Roberto Ortega <261643698+rotprods@users.noreply.github.com>
2026-08-30 14:02:04 +0200

    feat(remotion): export S04 golden-scene module

---
+++ runtime/remotion/src/golden_s04/index.ts
@@ -0,0 +1,3 @@
+export {S04Cientificamente} from './S04Cientificamente';
+export {S04_DEFAULT_PROPS, S04_SPEC} from './s04Spec';
+export type {S04CientificamenteProps, S04PlateMode} from './s04Spec';
